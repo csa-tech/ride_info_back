c83c461575a5c9182dc3514b3c9fac41e708ab52
Alex Xu <akxu@umail.ucsb.edu>
2020-02-18 20:27:39 -0800

    Fixed program crash in the event of SQL query errors

--- routes/users.js
+++ routes/users.js
@@ -1,45 +1,45 @@
 var express = require('express');
+var mysql = require('mysql');
 var router = express.Router();
-
-var userDao=require('../dao/userDao');
 
 /* GET users listing. */
 
-router.post('/',(req,res,next)=>{
+var secret = {
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD
+}
+
+router.post('/', (req, res, next)=>{
+
     console.log(req.body);
 
-    var mysql      = require('mysql');
-
-
-    var connection = mysql.createConnection({host:'mydatabase.c9ukuxyqda4n.us-west-1.rds.amazonaws.com',user:'CSAUser',password:'Csa666!!',database: 'rideshare'});
+    var connection = mysql.createConnection(secret);
     connection.connect();
 
     console.log(req.body);
-    try {
-          // connection.query('INSERT INTO rideshare.application(ride_id,passenger_id,status,num_passenger,note) VALUES(\''+req.body.ride_id+'\',\''+req.body.passenger_id+'\',\''+req.body.status+'\',\''+req.body.wechat_id+'\',\''+req.body.note+'\'); ', function(err, rows, fields) 
-          connection.query('INSERT INTO rideshare.application(ride_id,passenger_id,status,num_passenger,note) VALUES ?',
-            [
-            [
-              [req.body.ride_id,
-              req.body.user_id,
-              "PENDING",
-              req.body.num_passenger,
-              req.body.note]
-            ]
-            ],
-           function(err, rows, fields){ //这里写SQL query
-            if (err) { throw err; } //出错时交给后面的finally去处理      
+    connection.query('INSERT INTO rideshare.application(ride_id,passenger_id,status,num_passenger,note) VALUES ?',
+        [
+        [
+            [req.body.ride_id,
+            req.body.user_id,
+            "PENDING",
+            req.body.num_passenger,
+            req.body.note]
+        ]
+        ],
 
-            // 成功时的处理
-            // rows = [ RowDataPacket { count: 0, update_time: 2019-01-15T23:34:59.000Z } ]
-            res.status(200).send('OK');
-          });
-        } catch(err) { // 这里使用try...catch语法，即使出bug也可以正常应答，并且断开和数据库的连接
-          res.status(500).send('SERVER ERROR:' + err); // SQL出错时的处理
-          connection.end();
-        }
-})
+        function(err, rows, fields) {
+            if (err) {
+                return res.status(500).send('SERVER ERROR: ' + err.code);
+                connection.end();
+            }
+            return res.status(200).send('OK');
+        }); 
 
-
+                // 成功时的处理
+                // rows = [ RowDataPacket { count: 0, update_time: 2019-01-15T23:34:59.000Z } ]
+        
+});
 
 module.exports = router;
